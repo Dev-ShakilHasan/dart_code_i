e3d45eaca7ff361f052a2c3471a8577cbaf11cb6
shakil-hasan <dev.shakilhasan22@gmail.com>
2023-04-17 00:55:15 +0600

    DartCodeI

---
+++ bin/list_practice.dart
@@ -0,0 +1,22 @@
+//This is a problem to solve:-
+//------------------------------------
+///Take a list of phone numbers example
+///phoneNumber = [ +88, 01768131685, 01768171985, 01768111286, 01768131685]
+///And write program that prints out the full number like ‘+8801768171985’
+///You have to print out all numbers given in the list except +88.
+//--------------------------------------------------------
+
+void main() {
+  print(phoneNumbers[0] + phoneNumbers[1]);
+  print(phoneNumbers[0] + phoneNumbers[2]);
+  print(phoneNumbers[0] + phoneNumbers[3]);
+  print(phoneNumbers[0] + phoneNumbers[4]);
+}
+
+List<String> phoneNumbers = [
+  '+88',
+  '01768131685',
+  '01768171985',
+  '01768111286',
+  '01768131685'
+];
