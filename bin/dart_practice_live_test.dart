acdface1092350d0984813129ca14a902a08d39b
shakil-hasan <dev.shakilhasan22@gmail.com>
2023-04-18 00:07:20 +0600

    A problem practice

---
+++ bin/dart_practice_live_test.dart
@@ -0,0 +1,15 @@
+//Create a map (key value pair) of an employee, the keys will be employee = [Id, Name, Age, Salary, Gender].
+// In this map [Id, Age, Salary] only these three keys' values will be in number.
+// At last print out the Salary from the map.
+
+void main() {
+  print(oneEmployeeDetails['Salary']);
+}
+
+Map<String, dynamic> oneEmployeeDetails = {
+  'Id': 1231234,
+  'Name': 'Md. Shakil Hasan',
+  'Age': 23,
+  'Salary': 45000,
+  'Gender': 'Male'
+};
